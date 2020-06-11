04c39ca4b12e4d642a5553d7481abe12eede60d1
zsj <zoushijun@threatbook.cn>
2020-06-11 15:17:09 +0800

    init branch

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zsjun/blog.git master:gh-pages
+git push -f git@github.com:zsjun/blog.git master:deploy
 # 例如 git push -f git@github.com:koala-coding/blog.git master:gh-pages
 cd -
